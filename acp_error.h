29a974f456cec8cbc69fb3f7046d9cc717c8989e
Tej Narayan Kunwar <tej@tej-scalarc>
2013-01-23 14:40:59 +0530

    Modify to reflect movement of custom data types such as on,off,true,false.ACP_OK etc. to acp_dtypes.h

--- acp_error.h
+++ acp_error.h
@@ -8,10 +8,15 @@
 #ifndef ACP_ERROR_H_
 #define ACP_ERROR_H_
 
-#define ACP_OK 0
+#ifndef ACP_DTYPES_H_
+	#include "acp_dtypes.h"
+#endif
+
 //standard error codes
 typedef enum {
     ACP_ERR_ACP_STATE_INIT = 2,
+    ACP_ERR_NO_CONFIG_FILE,
+    ACP_ERR_CONFIG_ABORT, /*Configuration is unacceptable, abort */
     ACP_ERR_INIT_UI ,
     ACP_ERR_WINDOW_SIZE_TOO_SHORT,
     ACP_ERR_NO_COLOR,
@@ -21,5 +26,9 @@ typedef enum {
     ACP_ERR_DRAW_WINDOWS,
     ACP_ERR_GENERIC
 } error_codes_t;
+
+typedef enum {
+    LOG_DEBUG, LOG_WARN, LOG_ERROR
+} log_level_t;
 
 #endif /* ACP_ERROR_H_ */
