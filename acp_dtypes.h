c350d44e54900b27b7ad1954307b6d633d08945d
Tej Narayan Kunwar <tej@tej-scalarc>
2013-01-23 14:37:22 +0530

    Defines custom data types for use in acp.

---
+++ acp_dtypes.h
@@ -0,0 +1,21 @@
+/*
+ * acp_dtypes.h
+ *
+ *  Created on: 23-Jan-2013
+ *      Author: tej
+ *  Description: Define custom data types that will be used universally
+ */
+
+#ifndef ACP_DTYPES_H_
+#define ACP_DTYPES_H_
+
+#define ACP_OK 0
+typedef enum{
+	off,on
+} ON_OFF;
+
+typedef enum {
+		false, true
+	} bool;
+
+#endif /* ACP_DTYPES_H_ */
