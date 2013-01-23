77296de8bac23be4005555b62d3b19cb24fd4e1e
Tej Narayan Kunwar <tej@tej-scalarc>
2013-01-23 14:40:46 +0530

    Modify to reflect movement of custom data types such as on,off,true,false.ACP_OK etc. to acp_dtypes.h

--- acp_config.h
+++ acp_config.h
@@ -10,6 +10,17 @@
 #ifndef _ACP_CONFIG_H_
 #define _ACP_CONFIG_H_
 
+#ifndef ACP_DTYPES_H_
+	#include "acp_dtypes.h"
+#endif
+
+#include<stdio.h>
+#include<unistd.h>
+#include <string.h>
+#include<libconfig.h>
+
+#include "acp_error.h"
+
 /*
  * the program's max. screen coordinates for our program
  */
@@ -17,9 +28,9 @@
 #define MIN_COLS 100
 
 /*
- * status windows wil be given preference over console window.
- * so first sizes of the two windows will be calcualted then console's size.
- * if needed, console size can be overrriden.
+ * status windows will be given preference over console window.
+ * so first sizes of the two windows will be calculated then console's size.
+ * if needed, console size can be overriden.
  *
  * Size is represented in no. of lines or rows.
  */
@@ -28,14 +39,21 @@
 #define CONSOLE_SIZE 6
 #define STATUS_WINDOW_SIZE 12
 
-#define ON 1
-#define OFF 0
-
 //modify initialization behaviour of acp
-#define RAW_MODE OFF
-#define NOECHO ON
-#define KEYPAD_MODE ON
-#define CBREAK_MODE ON
+#define RAW_MODE off
+#define NOECHO on
+#define KEYPAD_MODE on
+#define CBREAK_MODE on
 
+#define ACP_CONF_FILE "acp.conf"
+
+struct acp_config_t {
+	int max_buff_size;
+	int swappiness;
+	bool cell_compaction_enabled;
+} acp_config;
+
+// --------routines-----------
+int load_config();
 #endif
 
